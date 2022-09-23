24d66534c2cd65fd03e0c17e0560a6cb67db77d6
RonaldBorda <ronald.borda@tecsup.edu.pe>
2022-09-22 22:42:28 -0500

    Add project files.

---
+++ Semana05/ManCategoria.xaml.cs
@@ -0,0 +1,80 @@
+﻿using Business;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Semana05
+{
+    /// <summary>
+    /// Interaction logic for ManCategoria.xaml
+    /// </summary>
+    public partial class ManCategoria : Window
+    {
+        public int ID { get; set; }
+        public ManCategoria(int iD)
+        {
+
+            InitializeComponent();
+            ID = iD;
+            if (ID > 0)
+            {
+                BCategoria bCategoria = new BCategoria();
+                List<Categoria> categorias = new List<Categoria>();
+                categorias = bCategoria.Listar(ID);
+                if (categorias.Count > 0)
+                {
+                    lblID.Content = categorias[0].IdCategoria.ToString();
+                    txtNombre.Text = categorias[0].NombreCategoria;
+                    txtDescripcion.Text = categorias[0].Descripcion;
+                }
+            }
+        }
+        private void BtnGrabar_Click(object sender, RoutedEventArgs e)
+        {
+            BCategoria BCategoria = null;
+            bool result = true;
+            try
+            {
+                BCategoria = new BCategoria();
+                if (ID > 0)
+
+                    result = BCategoria.Actualizar(new Categoria { IdCategoria = ID, NombreCategoria = txtNombre.Text });
+
+
+                else
+
+                    result = BCategoria.Insertar(new Categoria { NombreCategoria = txtNombre.Text, Descripcion = txtDescripcion.Text });
+
+
+                if (!result)
+                    MessageBox.Show("Comunicarse con el administrador aquiii");
+
+                Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Comunicarse con el administrador ");
+            }
+            finally
+            {
+                BCategoria = null;
+            }
+
+        }
+        private void BtnCerrar_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
